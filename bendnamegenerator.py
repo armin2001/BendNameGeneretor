28a70bc457147a61472a0517a43b2628d45bb3bc
Armin Imamović <69986688+armin2001@users.noreply.github.com>
2021-02-26 16:25:52 +0100

    Add files via upload

---
+++ bendnamegenerator.py
@@ -0,0 +1,11 @@
+#This is my first python app that i created
+print("This is the bend name generator")
+
+# Write the name of the city where you live
+city = input("In which city to you live in: \n")
+
+# Write the name of the pet you had
+pet = input("Write name of your pet: \n")
+
+# Combine the name of their city and pet and show them their band name
+print("Your band name could be: " + city + " " + pet)
